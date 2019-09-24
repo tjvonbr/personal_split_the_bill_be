6566c325c7516beee42898357e7fa61029d52daf
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-09-23 20:59:47 -0400

    function CRUD operation in meals table.

--- meals/mealsRouter.js
+++ meals/mealsRouter.js
@@ -24,7 +24,7 @@ router.get('/:id', (req, res) => {
         })
         .catch(err => {
             console.log(err)
-            res.status(500).json({ message: 'oops server needs maintenance, contact @Trever or @Ryan' })
+            res.status(500).json({ message: 'oops something happened' })
         })
     }
 })
@@ -33,7 +33,7 @@ router.post('/', (req, res) => {
     const { restaurant, meal, total, comments } = req.body
     const post = req.body
     if( !restaurant || !meal || !total ) {
-        res.status(400).json({ message: 'Must contain restaurant, meal, total'})
+        res.status(400).json({ message: 'Must contain: restaurant, meal, total'})
     } else {
         mealsDb.insert(post, 'id')
         .then( meals => {
@@ -41,18 +41,34 @@ router.post('/', (req, res) => {
         })
         .catch(err => {
             console.log(err)
-            res.status(500).json({ message: 'oops server needs maintenance, contact @Trever or @Ryan' })
+            res.status(500).json({ message: 'oops something happened' })
         })
     }
     
 })
 
 router.put('/:id', (req, res) => {
-    
+    const { id } = req.params
+    const { restaurant, meal, total, comments } = req.body
+    const post = req.body
+    if( !restaurant || !meal || !total || !comments ) {
+        res.status(400).json({ message: 'Must contain: restaurant, meal, total'})
+    } else {
+        mealsDb.update(id, post)
+            .then( meals => {
+                res.status(201).json(meals)
+            })
+    }        
 })
 
 router.delete('/:id', (req, res) => {
-    
+    const { id } = req.params
+    mealsDb.remove(id)
+        .then(res => {res.status(204).json(user)})
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'oops something happened' })
+        })
 })
 
 module.exports = router
