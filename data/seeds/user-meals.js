a1ca524852197caa29aec7bf22b26b7d714fa8e2
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-09-25 15:45:56 -0400

    changed seed back

--- data/seeds/user-meals.js
+++ data/seeds/user-meals.js
@@ -19,7 +19,12 @@ exports.seed = function(knex) {
           user_id: 2,
           meal_id: 1,
           paid: false
-        },       
+        },
+        {
+          user_id: 2,
+          meal_id: 2,
+          paid: false 
+        }      
       ]);
     });
 };
